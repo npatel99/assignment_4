07ebd351e400988a7c58813ed3ea33242a07cbbc
npatel99 <50912904+npatel99@users.noreply.github.com>
2022-07-16 11:10:52 +0530

    print first 10 natural numbers

---
+++ prog_2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/*Write a program to print the first 10 natural numbers.*/
+int main()
+{
+  int i = 1;
+  printf("first 10 natural numbers are: ");
+  while(i<=10)
+  {
+    printf("%d ", i);
+    i++;
+  }
+  printf("\n");
+
+  return 0;
+}
